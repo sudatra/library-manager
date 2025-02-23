860dfc811aef6c1212c219ac1433900a3d7e89e5
sudatra <mail2sudatra@gmail.com>
2025-02-23 18:10:02 +0530

    added book-overview and cover components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
       {/* <BookList 
         title='Latest Books'
         books={sampleBooks}
-        containerClassName='mt-28 '
+        containerClassName='mt-28'
       /> */}
     </>
   );
