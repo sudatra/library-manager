fc336e4e9ba93fa62ea3057ca30a0758fa87b46e
sudatra <mail2sudatra@gmail.com>
2025-02-25 23:10:25 +0530

    added neon connection and fixed ui

--- lib/config.ts
+++ lib/config.ts
@@ -6,7 +6,8 @@ const config = {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!
-    }
+    },
+    databaseUrl: process.env.DATABASE_URL
   }
 };
 
