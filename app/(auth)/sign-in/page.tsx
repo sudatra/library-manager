2ea191e8ceb36f3df03f25c788c44d635712f7fe
sudatra <mail2sudatra@gmail.com>
2025-02-27 19:59:34 +0530

    added sign-in sign-up server actions

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import AuthForm from '@/components/AuthForm'
+import { signInWithCredentials } from '@/lib/actions/auth.actions'
 import { signInSchema } from '@/lib/validations'
 import React from 'react'
 
@@ -13,7 +14,7 @@ const SignIn = () => {
         email: '',
         password: ''
       }}
-      onSubmit={() => {}}
+      onSubmit={signInWithCredentials}
     />
   )
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import AuthForm from '@/components/AuthForm'
+import { signUp } from '@/lib/actions/auth.actions'
 import { signUpSchema } from '@/lib/validations'
 import React from 'react'
 
@@ -16,7 +17,7 @@ const SignUp = () => {
         universityId: 0,
         universityCard: ''
       }}
-      onSubmit={() => {}}
+      onSubmit={signUp}
     />
   )
 }
