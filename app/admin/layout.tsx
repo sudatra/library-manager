2743a1c118cdb1068ae2ce40bbb84e55946f3d45
sudatra <mail2sudatra@gmail.com>
2025-03-06 21:00:52 +0530

    added admin privileges

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,11 +4,26 @@ import React from 'react';
 import '@/styles/admin.css';
 import Sidebar from '@/components/admin/Sidebar';
 import Header from '@/components/admin/Header';
+import { db } from '@/database/drizzle';
+import { users } from '@/database/schema';
+import { eq } from 'drizzle-orm';
 
 const AdminLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
   if(!session?.user?.id) {
     redirect('/sign-in');
+  }
+
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === 'ADMIN')
+  ;
+
+  if(!isAdmin) {
+    redirect('/')
   }
 
   return (
