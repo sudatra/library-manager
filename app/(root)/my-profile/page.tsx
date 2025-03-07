90ce4301d21577af9e5adadd319859ef6a3fb5c8
sudatra <mail2sudatra@gmail.com>
2025-03-07 21:13:55 +0530

    added seeder file and function for database seeding

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,10 +1,14 @@
 import { signOut } from '@/auth';
 import BookList from '@/components/BookList';
 import { Button } from '@/components/ui/button'
-import { sampleBooks } from '@/constants';
+import { db } from '@/database/drizzle';
+import { books } from '@/database/schema';
+import { desc } from 'drizzle-orm';
 import React from 'react'
 
-const Profile = () => {
+const Profile = async () => {
+  const latestBooks = (await db.select().from(books).limit(10).orderBy(desc(books.createdAt))) as Book[];
+
   return (
     <>
       <form 
@@ -19,7 +23,7 @@ const Profile = () => {
 
       <BookList 
         title='Borrowed Books'
-        books={sampleBooks}
+        books={latestBooks.slice(1)}
       />
     </>
   )
--- components/BookList.tsx
+++ components/BookList.tsx
@@ -8,17 +8,17 @@ interface BookListProps {
 }
 
 const BookList = ({ title, books, containerClassName }: BookListProps) => {
-  if(books.length < 2) {
+  if(books?.length < 2) {
     return;
   }
-  
+
   return (
     <section className={containerClassName}>
       <h2 className='font-bebas-neue text-4xl text-light-100'>{title}</h2>
       
       <ul className='book-list'>
         {
-          books.map((book) => (
+          books?.map((book) => (
             <BookCard 
               key={book.title}
               {...book}
